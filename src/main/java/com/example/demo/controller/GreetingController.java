5c37b1e84a5cef243f15762e4771128bb1419088
CharlyBrownRiv <charlialan@hotmail.com>
2021-11-26 17:16:42 -0600

    mi primer commit

---
+++ src/main/java/com/example/demo/controller/GreetingController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+
+import com.example.demo.message.HelloMessage;
+import com.example.demo.model.Greeting;
+
+@Controller
+public class GreetingController {
+
+	  @MessageMapping("/hello")
+	  @SendTo("/topic/greetings")
+	  public Greeting greeting(HelloMessage message) throws Exception {
+	    Thread.sleep(1000); // simulated delay
+	    return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+	  }
+	
+}
